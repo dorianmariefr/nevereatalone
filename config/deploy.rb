0b2552b5f29f6aa57a5b6dc1f4d04ff414687d4e
Dorian Marié <dorian.marie.france@gmail.com>
2020-03-15 14:24:51 +0100

    start of home website

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.12.1"
 
-set :application, "template-capistrano"
-set :repo_url, "git@github.com:dorianmariefr/template-capistrano.git"
+set :application, "dorianmarie.fr"
+set :repo_url, "git@github.com:dorianmariefr/dorianmarie.fr.git"
 set :user, "ec2-user"
 set :deploy_to, -> { "/home/#{fetch(:user)}/applications/#{fetch(:application)}" }
 
