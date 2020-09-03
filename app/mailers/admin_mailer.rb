c6c883769dbce427380d2acd7aa0efb9bfcdd211
Dorian Marié <dorian@dorianmarie.fr>
2020-09-04 00:30:55 +0200

    simpler admin email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,8 +2,7 @@ class AdminMailer < ApplicationMailer
   def admin_notification_email
     @model = params[:model]
     @user = params[:user]
-    subject = "#{@model.class}##{@model.id} changed: #{@model.to_s}"
 
-    mail(to: @user.email, subject: subject)
+    mail(to: @user.email, subject: "#{@model.class}##{@model.id}: #{@model.to_s}")
   end
 end
