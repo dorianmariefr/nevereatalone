64a6cef030ba43b1f65d7e049a342ba96831ecd6
Dorian Marié <dorian@dorianmarie.fr>
2020-09-03 15:17:50 +0200

    add .env.staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, "git@github.com:dorianmariefr/nevereatalone.git"
 set :user, "ec2-user"
 set :deploy_to, -> { "/home/#{fetch(:user)}/applications/#{fetch(:application)}" }
 
-append :linked_files, '.env.production'
+append :linked_files, ".env.production", ".env.staging"
 append :linked_dirs, 'tmp/pids', 'tmp/sockets', 'log'
 
 set :format_options, truncate: false
