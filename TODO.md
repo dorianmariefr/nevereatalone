39a81345d3067f49b7179eed6a9b93f00950f3ae
Dorian Marié <dorian@dorianmarie.fr>
2020-09-04 23:38:37 +0200

    can read user for logged in users

--- TODO.md
+++ TODO.md
@@ -2,6 +2,7 @@
 
 - reminder on dashboard to add image, bio and center of interests
 - current and past availabilities on user page
+- round images
 
 ### later
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
     if user.admin?
       can :manage, :all
     else
+      can :read, User
       can :manage, User, id: user.id
 
       can :read, Availability
