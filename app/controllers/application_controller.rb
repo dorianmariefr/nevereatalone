f86730be03c635d16955ebdc25f5377e0682733f
Dorian Marié <dorian@dorianmarie.fr>
2020-09-06 15:41:06 +0200

    try without the csrf fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true, with: :exception
+  # protect_from_forgery prepend: true, with: :exception
 
   check_authorization unless: :admin_controller?
 
