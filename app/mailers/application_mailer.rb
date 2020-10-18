e0717393684fea4ff97d4f456ffe99cb8ce8da62
Dorian Marié <dorian@dorianmarie.fr>
2020-10-18 23:54:41 +0200

    try adding images

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "pas-de-reponse@jamaismangerseul.fr"
   layout "mailer"
+
+  before_action do
+    attachments.inline['logo.png'] = File.read(Rails.root.join("app/assets/images/logo.png"))
+  end
 end
 
