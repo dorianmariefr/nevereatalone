228e7f41b8c5faa55a9391be1c237c2aa2e1426b
Dorian Marié <dorian@dorianmarie.fr>
2020-10-25 16:08:50 +0100

    fix facebook images

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :facebook,
     ENV.fetch("FACEBOOK_APP_ID"),
     ENV.fetch("FACEBOOK_APP_SECRET"),
-    image_size: :large
+    image_size: :large,
+    secure_image_url: true,
   )
 end
